b69fb17d1bfd18b8a2f043c76ea13ad8dd88215e
細田 来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-09-29 07:26:39 +0900

    5-43

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 // import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert("OK");
+  };
+
+  const constentStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={constentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
