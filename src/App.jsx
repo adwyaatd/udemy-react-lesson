73234bcccfa3df466c72d7e2b5a9a9ac1fc3a98b
細田 来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-09-30 08:18:46 +0900

    5-46

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./componets/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    console.log("useeffect");
+    if (num > 0 && num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">お元気ですか？</ColorfulMessage>
       <button onClick={onClickCountUp}>ボタン</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
